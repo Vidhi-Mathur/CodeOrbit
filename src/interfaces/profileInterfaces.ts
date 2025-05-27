56fc305aa08dcd2c384793158fd1b38682c9e244
Vidhi <mathurvidhi2505@gmail.com>
2025-05-27 22:40:04 +0530

    Added leetcode interface

--- src/interfaces/profileInterfaces.ts
+++ src/interfaces/profileInterfaces.ts
@@ -19,13 +19,55 @@ export interface GitHubRepoInterface {
     topics: string[]
 }
 
-
 export interface GitHubProfileProps {
     profile: GitHubProfileInterface
 }
 
 export interface GitHubActivityProps {
     username: string
+}
+
+export interface LeetCodeProfileInterface {
+    username: string
+    realName: string
+    aboutMe: string | null
+    ranking: number
+    reputation: number,
+    skillTags: string[],
+    badges: LeetCodeBadge[],
+    submissionStats: LeetCodeSubmissionStat[],
+    languageStats: LeetCodeLanguageStat[],
+    submissionCalendar: Record<string, string>
+}
+
+export interface LeetCodeProfileProps {
+    profile: LeetCodeProfileInterface
+}
+
+
+interface LeetCodeBadge {
+    id: string;
+    name: string;
+    icon: string;
+}
+
+interface LeetCodeSubmissionStat {
+    difficulty: 'All' | 'Easy' | 'Medium' | 'Hard';
+    count: number;
+    submissions: number;
+}
+
+interface LeetCodeLanguageStat {
+    languageName: string;
+    problemsSolved: number;
+}
+
+export interface LeetcodeContestInterface {
+    totalContestAttended: number
+    globalRanking: number
+    rating: number
+    topPercentage: number
+    totalParticipants: number
 }
 
 export interface CurvedNavProps {
