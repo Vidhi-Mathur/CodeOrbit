b3895cf207841819cd6ecf66dbbdc4f51ebc7c7d
Vidhi <mathurvidhi2505@gmail.com>
2025-05-27 22:39:10 +0530

    Replaced link with button

--- src/components/dsa/DSAStats.tsx
+++ src/components/dsa/DSAStats.tsx
@@ -1,14 +1,13 @@
 import { cpLinks } from "@/constants/profileConstant"
 import Image from "next/image"
-import Link from "next/link"
 
-const DSAStats = () => {
+const DSAStats = ({ onClick }: { onClick: (info: string) => void }) => {
     return (
         <div className="flex flex-col gap-2 mt-4 ml-2">
                 {cpLinks.map((info) => ( 
-                    <Link href="/" key={info} className="p-3 w-14 rounded-2xl shadow-md hover:shadow-lg transition-shadow bg-white flex items-center justify-center">
+                    <button key={info} onClick={() => onClick(info)} className="p-3 w-14 rounded-2xl shadow-md hover:shadow-lg transition-shadow bg-white flex items-center justify-center">
                         <Image src={`/dsa/${info}.svg`} width={24} height={24} alt={info} />
-                    </Link>
+                    </button>
                 ))}
         </div>
     )
