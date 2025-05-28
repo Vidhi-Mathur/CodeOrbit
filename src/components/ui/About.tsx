2ac8d668d4ee283804f7def3ec02bc9617bc236d
Vidhi <mathurvidhi2505@gmail.com>
2025-05-28 20:38:56 +0530

    Separate components for about and profile section

---
+++ src/components/ui/About.tsx
@@ -0,0 +1,46 @@
+import { infoLinks } from "@/constants/profileConstant"
+import { AboutProps } from "@/interfaces/profileInterfaces";
+import Image from "next/image"
+import Link from "next/link";
+
+export const About = ({ name = "User's Name", username = "User's Username", image, email, education, info }: AboutProps) => {
+    let getRedirection = (key: string): string => {
+        switch (key) {
+            case "email":
+                return `mailto:${email}`;
+            case "linkedin":
+                return `https://www.linkedin.com/in/${info.linkedin}` || "/";
+            case "twitter":
+                return `https://x.com/${info.X}` || "/";
+            case "website":
+                return `${info.website}` || "/";
+            default:
+                return "/";
+        }
+    }
+    return (
+        <div className="text-black">
+            <div className="flex flex-col md:flex-row items-start md:items-start gap-2 justify-between">
+                <div className="flex flex-col gap-2">
+                    <div className="relative rounded-full w-24 h-24 overflow-hidden">
+                        <Image className="object-cover" src={image || "/bg/profile.png"} alt="Profile" fill />
+                    </div>
+                    <div className="mt-2">
+                        <h2 className="text-xl font-semibold">{name}</h2>
+                        <p className="text-gray-600">@{username}</p>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 -mt-4 md:-mt-4">
+                    {infoLinks.map((info) => { 
+                        const href = getRedirection(info)
+                        return (
+                            <Link href={href} key={info} className="p-3 rounded-2xl shadow-md hover:shadow-lg transition-shadow bg-white flex items-center justify-center">
+                                <Image src={`/common/${info}.svg`} width={24} height={24} alt={info} />
+                            </Link>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/ui/ProfileComponent.tsx
@@ -0,0 +1,71 @@
+"use client"
+import DevStats from "@/components/dev/DevStats"
+import { GitHubProfile } from "@/components/dev/github/GitHubProfile"
+import { GitHubRepo } from "@/components/dev/github/GitHubRepo"
+import DSAStats from "@/components/dsa/DSAStats"
+import { LeetCodeProfile } from "@/components/dsa/leetcode/LeetCodeProfile"
+import { About } from "@/components/ui/About"
+import { CurvedNav } from "@/components/ui/CuvedNav"
+import { ShimmerProfile, ShimmerRepo } from "@/components/ui/ShimmerUI"
+import { profileConstants } from "@/constants/profileConstant"
+import { useGitHub } from "@/hooks/useGitHub"
+import { useLeetCode } from "@/hooks/useLeetCode"
+import { ProfileComponentProps } from "@/interfaces/profileInterfaces"
+import { useEffect, useState } from "react"
+
+const ProfileComponent = ({ user }: ProfileComponentProps) => {
+  const [activeTab, setActiveTab] = useState(profileConstants[0].image)
+  const { githubProfile, repos, loading: githubLoading, error: githubError, fetchGitHubData } = useGitHub(user.platforms.dev.github)
+  const { leetcodeProfile, loading: leetcodeLoading, error: leetcodeError, fetchLeetCodeData } = useLeetCode(user.platforms.dsa.leetcode)
+
+  useEffect(() => {
+    if (activeTab === profileConstants[0].image) fetchLeetCodeData()
+    if (activeTab === profileConstants[1].image) fetchGitHubData()
+  }, [activeTab])
+
+      return (
+        <div className="flex h-screen w-full overflow-hidden">
+          <div className="flex flex-col w-full md:w-3/4">
+              <div className="p-6 bg-white h-[calc(5/12*100vh)] pt-[80px] relative">
+                  <About name={user.name} username={user.username} image={user.image} email={user.email} education={user.education} info={user.platforms.others}/>
+                  <div className="absolute -bottom-4 left-0 right-0 z-10">
+                    <CurvedNav activeTab={activeTab} setActiveTab={setActiveTab}/>
+                  </div>
+              </div>
+              <div className="bg-blue-200 h-[calc(3/4*100vh)] overflow-y-auto scrollbar-hide">
+                  {activeTab === profileConstants[0].image && (
+                      <>
+                      <DSAStats onClick={fetchLeetCodeData}/>
+                      <div className="flex-1 m-6 -mt-95">
+                          {leetcodeLoading && <ShimmerProfile />}
+                          {leetcodeError && <div className="-mt-20 bg-red-50 ml-12 p-4 rounded-lg border border-red-200 text-red-700">{leetcodeError}</div>}
+                          {!leetcodeLoading && !leetcodeError && leetcodeProfile && <LeetCodeProfile profile={leetcodeProfile} />}
+                      </div>
+                      </>
+                  )}
+                  {activeTab === profileConstants[1].image && (
+                      <>
+                      <DevStats onClick={fetchGitHubData} />
+                      <div className="flex-1 m-6">
+                          {githubLoading && <ShimmerProfile />}
+                          {githubError && <div className="-mt-20 bg-red-50 ml-12 p-4 rounded-lg border border-red-200 text-red-700">{githubError}</div>}
+                          {!githubLoading && !githubError && githubProfile && <GitHubProfile profile={githubProfile} />}
+                      </div>
+                      </>
+                  )}
+              </div>
+          </div>
+          <div className="hidden md:block md:w-1/4 bg-cyan-200 pt-[80px] overflow-y-auto h-screen scrollbar-hide">
+              {activeTab === profileConstants[1].image && (
+                  <>
+                  {githubLoading && <ShimmerRepo />}
+                  {githubError && <div className="bg-red-50 p-4 ml-2 mr-2 rounded-lg border border-red-200 text-red-700">{githubError}</div>}
+                  {!githubLoading && !githubError && repos && <GitHubRepo repos={repos} />}
+                  </>
+              )}
+          </div>
+        </div>
+      )
+  }
+  
+  export default ProfileComponent
