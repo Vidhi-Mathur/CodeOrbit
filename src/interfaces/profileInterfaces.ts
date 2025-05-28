2abf4d5eb413291931dd165a91abd77457670766
Vidhi <mathurvidhi2505@gmail.com>
2025-05-28 20:41:53 +0530

    Added interfaces

--- src/interfaces/profileInterfaces.ts
+++ src/interfaces/profileInterfaces.ts
@@ -74,3 +74,55 @@ export interface CurvedNavProps {
     activeTab: string;
     setActiveTab: (value: string) => void;
 }
+
+export interface AboutProps {
+    name?: string | null
+    username?: string | null
+    image?: string | null
+    email: string
+    education: EducationInterface
+    info: InfoInterface
+}
+
+export interface ProfileComponentProps {
+    user: {
+        name: string
+        email: string
+        image: string
+        authProvider: string
+        authProviderId: string
+        username: string, 
+        isOnboarded: boolean
+        education: EducationInterface,
+        platforms: {
+            dsa: {
+                leetcode: string
+                geeksforgeeks: string
+                codeforces: string
+                codechef: string
+                hackerrank: string
+                interviewbit: string
+                codingninjas: string
+            }
+            dev: {
+                github: string
+            }
+            others: InfoInterface
+        }
+    }
+};
+
+interface EducationInterface {
+    degree: string
+    branch: string
+    college: string
+    gradYear: string
+    location: string
+    current_profile: string
+}
+
+interface InfoInterface {
+    website: string,
+    linkedin: string,
+    X: string
+}
