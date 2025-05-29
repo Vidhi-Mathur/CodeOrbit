cdc6e3e8f6eb6cf955b07553624c1e7e3de443b8
Vidhi <mathurvidhi2505@gmail.com>
2025-05-29 20:40:17 +0530

    Adjusted education section

--- src/components/ui/About.tsx
+++ src/components/ui/About.tsx
@@ -1,5 +1,6 @@
 import { infoLinks } from "@/constants/profileConstant"
 import { AboutProps } from "@/interfaces/profileInterfaces";
+import { Dot, GraduationCap, MapPinIcon } from "lucide-react";
 import Image from "next/image"
 import Link from "next/link";
 
@@ -28,6 +29,44 @@ export const About = ({ name = "User's Name", username = "User's Username", imag
                     <div className="mt-2">
                         <h2 className="text-xl font-semibold">{name}</h2>
                         <p className="text-gray-600">@{username}</p>
+                    </div>
+                </div>
+                <div className="flex-1 md:mx-6">
+                    <div className="relative bg-gradient-to-br from-blue-50 via-cyan-50 to-blue-100 p-4 rounded-2xl shadow-lg border border-blue-100/50 hover:shadow-xl transition-all duration-300">
+                        <div className="flex flex-col md:flex-row justify-between gap-4">
+                            <div className="space-y-3 md:w-[65%]">
+                                <div className="flex items-start gap-2">
+                                    <GraduationCap className="w-6 h-6" />
+                                    <div>
+                                        <h3 className="font-medium text-gray-900">{education.degree}</h3>
+                                        <h3 className="text-gray-500 flex items-center">
+                                            <span className="inline-flex items-center mr-1"><Dot /></span>
+                                            With Specialization in {education.branch}
+                                        </h3>
+                                        <h3 className="text-gray-500 flex items-center">
+                                            <span className="inline-flex items-center mr-1"><Dot /></span>
+                                            {`${Number(education.gradYear) < new Date().getFullYear() ? "Graduated in" : "To be graduated in"} ${education.gradYear}`}
+                                        </h3>
+                                        <h3 className="text-gray-700 flex items-center">
+                                            <span className="inline-flex items-center mr-1"><Dot /></span>
+                                            From {education.college}
+                                        </h3>
+                                    </div>
+                                </div>
+                                <div className="flex items-start gap-2">
+                                    <Image src="/common/current_profile.svg" width={20} height={20} alt="Currentprofile" className="ml-1" />
+                                    <div>
+                                        <h3 className="font-medium text-gray-900">{education.current_profile}</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex items-start gap-2 md:w-[30%] md:ml-auto md:mr-4">
+                                <MapPinIcon className="w-6 h-6 mt-1" />
+                                <div>
+                                    <h3 className="font-medium text-gray-900 mt-1">{education.location}</h3>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="flex flex-col gap-2 -mt-4 md:-mt-4">
