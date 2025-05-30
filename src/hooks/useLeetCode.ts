10e743069e678a6676df62dd39f0eace99ece571
Vidhi <mathurvidhi2505@gmail.com>
2025-05-30 23:38:29 +0530

    Added submission calendar

--- src/hooks/useLeetCode.ts
+++ src/hooks/useLeetCode.ts
@@ -1,11 +1,12 @@
 "use client"
-import { LeetcodeContestInterface, LeetCodeProfileInterface } from "@/interfaces/profileInterfaces"
+import { LeetcodeCalendarInterface, LeetcodeContestInterface, LeetCodeProfileInterface } from "@/interfaces/dsa/leetcode/leetcodeInterface"
 import axios from "axios"
 import { useState } from "react"
 
 export const useLeetCode = (leetcodeUsername: string) => {
     const [leetcodeProfile, setLeetcodeProfile] = useState<LeetCodeProfileInterface | null>(null)
     const [contest, setContest] = useState<LeetcodeContestInterface>();
+    const [submissionCalendar, setSubmissionCalendar] = useState<LeetcodeCalendarInterface | null>(null)
     const [loading, setLoading] = useState<boolean>(false)
     const [error, setError] = useState<string | null>(null)
     const fetchLeetCodeData = async() => {
@@ -13,9 +14,10 @@ export const useLeetCode = (leetcodeUsername: string) => {
         setError(null)
         try {
             const response =  await axios.get(`/api/dsa/leetcode/${leetcodeUsername}`)
-            const { profileResponse, contestResponse } = response.data
+            const { profileResponse, contestResponse, submissionCalendarResponse } = response.data
             setLeetcodeProfile(profileResponse)
             setContest(contestResponse)
+            setSubmissionCalendar(submissionCalendarResponse)
         } 
         catch(err: any){
             console.error("API call failed", err)
@@ -26,5 +28,5 @@ export const useLeetCode = (leetcodeUsername: string) => {
             setLoading(false)
         }
     }
-    return { leetcodeProfile, contest, loading, error, fetchLeetCodeData }
+    return { leetcodeProfile, contest, submissionCalendar, loading, error, fetchLeetCodeData }
 }
