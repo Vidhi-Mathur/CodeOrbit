c591df18bb52e322db60d6f1213dc8512a06588f
Vidhi <mathurvidhi2505@gmail.com>
2025-06-04 23:28:10 +0530

    Added loading spinner

--- src/components/ui/ShimmerUI.tsx
+++ src/components/ui/ShimmerUI.tsx
@@ -161,5 +161,5 @@ export const ShimmerContest = () => {
 }
 
 export const LoadingSpinner = () => {
-    return <div className="w-6 h-6 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    return <div className="w-5 h-5 border-2 border-t-transparent border-[#03357a] rounded-full animate-spin ml-1" />
 }
