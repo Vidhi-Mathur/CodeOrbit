3f0e0bb64b7569bad07ca157b94da983469de45b
Vidhi <mathurvidhi2505@gmail.com>
2025-06-06 09:00:35 +0530

    Fixed overflow issue in sm screens

--- src/authOptions.ts
+++ src/authOptions.ts
@@ -60,6 +60,12 @@ export const authOptions: AuthOptions = {
             clientId: process.env.TWITTER_CLIENT_ID!,
             clientSecret: process.env.TWITTER_CLIENT_SECRET!,
             version: "2.0",
+            authorization: {
+                url: "https://twitter.com/i/oauth2/authorize",
+                    params: {
+                        scope: "tweet.read users.read offline.access",
+                },
+            },
         })
     ],
     pages: {
