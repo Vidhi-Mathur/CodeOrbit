9c157357d349e01fa6e37e07d2a0055954873610
Vidhi <mathurvidhi2505@gmail.com>
2025-06-10 20:53:10 +0530

    Renamed links

--- src/constants/profileConstant.ts
+++ src/constants/profileConstant.ts
@@ -5,8 +5,8 @@ export const months: string[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"
 export const infoLinks: string[] = ["email", "linkedin", "twitter", "website"] as const 
 export type InfoLink = typeof infoLinks[number]
 
-export const cpLinks: string[] = ["leetcode", "geeksforgeeks", "codeforces", "codechef", "hackerrank", "interviewbit", "codingninjas"] as const 
-export type CpLink = typeof cpLinks[number];
+export const dsaLinks: string[] = ["leetcode", "geeksforgeeks", "codeforces", "codechef", "hackerrank", "interviewbit", "codingninjas"] as const 
+export type DsaLink = typeof dsaLinks[number];
 
 export const devLinks: string[] = ["github"] as const 
 export type DevLink = typeof devLinks[number]
