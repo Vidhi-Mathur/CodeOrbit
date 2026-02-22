2f5d6ab4b9d47bb6cec9450b80aafc117d817de3
Vidhi <mathurvidhi2505@gmail.com>
2026-02-22 22:52:30 +0530

    Modified attribute name

--- src/components/ui/ProfileHeader.tsx
+++ src/components/ui/ProfileHeader.tsx
@@ -6,7 +6,7 @@ export const ProfileHeader = ({ user, activeTab, onTabChange, onRefresh, refresh
     return (
         <div className="relative p-3 sm:p-4 lg:p-6 bg-white lg:h-[calc(5/12*100vh)] pt-[72px] sm:pt-[70px] lg:pt-[80px] pb-16 sm:pb-16 lg:pb-0">  
             <div className="flex flex-col lg:block">
-                <About name={user.name} username={user.username} image={user.image} email={user.email} education={user.education} info={user.platforms.others} />
+                <About name={user.name} username={user.username} image={user.image} email={user.email} education={user.education} info={user.platforms.social} />
                 <div className="flex flex-row items-center gap-3 absolute bottom-8 left-1/2 -translate-x-1/2 lg:bottom-4 lg:left-4 lg:translate-x-0">
                     <button onClick={onRefresh} disabled={refreshState.status === "loading"} className={`px-6 py-2 text-sm font-medium tracking-wide rounded-xl bg-gradient-to-r from-[#1A1B2E] to-[#23244a] text-white shadow-md transition-all duration-200 ${refreshState.status === "loading"? "opacity-70 cursor-not-allowed": "hover:-translate-y-0.5 hover:shadow-lg"}`}>
                         {refreshState.status === "loading" ? "Refreshing..." : "Refresh"}
